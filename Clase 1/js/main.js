483061b5c84dcc4d57685bd709c83a470b2096b0
Luciano <wilbergerluciano15@gmail.com>
2021-09-06 11:18:13 -0300

    Primeras clases

---
+++ Clase 1/js/main.js
@@ -0,0 +1,11 @@
+let num1 = 20;
+let num2 = 15;
+
+let sumaDeNums = num1 + num2;
+
+console.log(sumaDeNums);
+
+
+
+
+
---
+++ Clase 2/main.js
@@ -0,0 +1,14 @@
+let edad = prompt("Ingresa tu edad");
+
+if(edad < 18){
+    alert("La edad es menor a 18");}
+
+    else if(edad == 18){
+        alert("La edad es 18");
+    }
+
+    else (edad > 18){
+        alert("La edad es mayor a 18");
+    }
+
+
