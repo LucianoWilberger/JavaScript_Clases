7b823b70b7748ee21c6d6b5d1bbe08d65083d37b
Luciano <wilbergerluciano15@gmail.com>
2021-09-07 02:10:51 -0300

    correciones clase 2

---
+++ Clase 2/js/main.js
@@ -0,0 +1,12 @@
+let edad = prompt("Ingresa tu edad");
+
+if(edad < 18){
+  alert("Menores de 18 años tienen prohibido el ingreso a la pagina")}
+else if(edad == 18,19,20,21,22,23,24){
+  alert("Puede entrar a la pagina")}
+ else if (edad >= 25){
+  alert("Acceso renstringido, la edad maxima permitida es 24")}
+
+
+
+
