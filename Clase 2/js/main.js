b01c0a49f8eb0d9c8e355a5a217ecf38632efc37
Luciano <wilbergerluciano15@gmail.com>
2021-09-08 02:54:12 -0300

    Desafio 2 hecho

--- Clase 2/js/main.js
+++ Clase 2/js/main.js
@@ -1,9 +1,24 @@
-let edad = prompt("Ingresa tu edad")
+let num1 = prompt("Ingrese un numero")
+let text = prompt("Ingrese un Hola");
+let num2 = prompt("Ingrese un numero entre 10 y 50")
 
-if(edad >=50){
-  alert("el numero es correcto")
-}else alert("el numero es incorrecto")
- 
+if(num1 >=1000){
+  alert("El numero ingresado es igual o mayor a 1000");
+} else if(num1 < 1000){
+  alert("El numero ingresado es menor a 1000");
+}
+
+if(text == "Hola"){
+  alert("Hola usuario ;)");
+} else if(alert("Saludame usuario"));
+
+if((num2 <=50) && (num2 >=10)){
+  alert("El numero ingresado esta entre 10 y 50");
+}else if(alert("El numero ingresado no esta entre 10 y 50"));
+
+
+
+
 
 
 
