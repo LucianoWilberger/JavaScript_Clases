d4a03d460819f8efbee1964e2b8ca5959ff9fee5
Luciano <wilbergerluciano15@gmail.com>
2021-09-22 21:44:21 -0300

    arreglo desafio 5

--- Clase 5 Objetos/js/main5.js
+++ Clase 5 Objetos/js/main5.js
@@ -1,6 +1,6 @@
 
-class SwordsMaker{
-    constructor (nameSword, type, resistance, damage, pitcher, zoneSpawn, radar){
+class SwordsMaker {
+    constructor(nameSword, type, resistance, damage, pitcher, zoneSpawn, radar) {
         this.nameSword = nameSword;
         this.type = type;
         this.resistance = resistance;
@@ -14,46 +14,55 @@ class SwordsMaker{
 }
 
 const nameSword = prompt('Elija una espada:\nSword of Caos\nSword Gladiator\nZombie Sword');
-function show(Message){
+
+function show(Message) {
     console.log(Message)
 }
 
-function swordChoise(nameSword){
-    switch(nameSword){
+function swordChoise(nameSword) {
+    switch (nameSword) {
         case "Sword of Caos":
-            show(caosSword)
+            alert("Has elegido la: " + caosSword.nameSword +"\nQue es de tipo: " + caosSword.type + "\nTiene una resistencia: " + caosSword.resistance  + "\nTiene un ataque base de: " + caosSword.damage + "\nLanza:" + caosSword.pitcher + "\nSe puede encontrar en: " + caosSword.zoneSpawn)
             break;
-        
+
         case "sword of caos":
-            show(caosSword)
-            break;    
-        
+            alert("Has elegido la: " + caosSword.nameSword +"\nQue es de tipo: " + caosSword.type + "\nTiene una resistencia: " + caosSword.resistance  + "\nTiene un ataque base de: " + caosSword.damage + "\nLanza:" + caosSword.pitcher + "\nSe puede encontrar en: " + caosSword.zoneSpawn)
+            bcaos
         case "Sword Gladiator":
-            show(gladiatorSword)
+            alert("Has elegido la: " + gladiatorSword.nameSword +"\nQue es de tipo: " + gladiatorSword.type + "\nTiene una resistencia: " + gladiatorSword.resistance  + "\nTiene un ataque base de: " + gladiatorSword.damage + "\nLanza:" + gladiatorSword.pitcher + "\nSe puede encontrar en: " + gladiatorSword.zoneSpawn)
             break;
-            
+
         case "sword gladiator":
-            show(gladiatorSword)
+            alert("Has elegido la: " + gladiatorSword.nameSword +"\nQue es de tipo: " + gladiatorSword.type + "\nTiene una resistencia: " + gladiatorSword.resistance  + "\nTiene un ataque base de: " + gladiatorSword.damage + "\nLanza:" + gladiatorSword.pitcher + "\nSe puede encontrar en: " + gladiatorSword.zoneSpawn)
             break;
-        
+
         case "Zombie Sword":
-            show(zombieSword)
+            alert("Has elegido la: " + zombieSword.nameSword +"\nQue es de tipo: " + zombieSword.type + "\nTiene una resistencia: " + zombieSword.resistance + "\nTiene un ataque base de: " + zombieSword.damage + "\nLanza:" + zombieSword.pitcher + "\nSe puede encontrar en: " + zombieSword.zoneSpawn)
             break;
         case "zombie sword":
-            show(zombieSword);
+            alert("Has elegido la: " + zombieSword.nameSword +"\nQue es de tipo: " + zombieSword.type + "\nTiene una resistencia: " + zombieSword.resistance + "\nTiene un ataque base de: " + zombieSword.damage + "\nLanza:" + zombieSword.pitcher + "\nSe puede encontrar en: " + zombieSword.zoneSpawn)
             break;
-            
+
         default:
-            show("No eligio ninguna de las espadas disponibles, su alma fue condenada")
-            break;    
+            alert("No eligio ninguna de las espadas disponibles, que son: " + Swords)
+            break;
     }
+
+
 
 
 }
 
-const gladiatorSword = new SwordsMaker('Gladiator Sword','Normal', 'low', '50', 'No', 'Market, Field, Chivalry, Castle', 'No');
-const caosSword = new SwordsMaker('Caos Sword','Very Rare','High', '300', 'Spit poison or fire', 'Kaos', 'Yes');
-const zombieSword = new SwordsMaker('Zombie Sword','Rare', 'Normal', '100', 'Spit weak posion', 'Graveyard, Dark forest, Haunted castle', 'No');
+
+
+const gladiatorSword = new SwordsMaker('Espada del Gladiador', 'Normal', 'Baja', '50', 'No', 'Market, Field, Chivalry, Castle', 'No');
+
+const caosSword = new SwordsMaker('Espada del Caos', 'Muy raro', 'Alta', '300', 'Escupe Veneno y Fuego', 'Kaos', 'Yes');
+
+const zombieSword = new SwordsMaker('Espada Zombie', 'Raro', 'Normal', '100', 'Escupe Veneno Debil', 'Graveyard, Dark forest, Haunted castle', 'No');
+
+const Swords = [gladiatorSword, caosSword, zombieSword];
+
 
 
 swordChoise(nameSword)
---
+++ Clase 6 Arrays/js/main.js
@@ -0,0 +1,9 @@
+class alimento {
+    constructor(nombre, tipo, color, kcal){
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.color = color;
+        this.kcal = kcal;
+    }
+}
+
